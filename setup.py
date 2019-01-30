013efb73843c3b057872d701aa8a9433b94b3158
Álvaro García <agarcia@bmat.com>
2019-01-30 12:07:34 +0100

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 setup(
     name='bazaar',
     packages=['bazaar'],
-    version='0.8',
+    version='0.9',
     description='Agnostic file storage',
     author='BMAT developers',
     author_email='tv-av@bmat.com',
