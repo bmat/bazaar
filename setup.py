c4ceba6e8af260e75341b8d6dce17529156d4ac4
Alvaro Garcia <agarcia@bmat.com>
2020-12-23 00:01:22 +0100

    Finally version 1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 setup(
     name='bazaar',
     packages=['bazaar'],
-    version='0.13',
+    version='1.0',
     description='Agnostic file storage',
     author='BMAT developers',
     author_email='tv-av@bmat.com',
@@ -15,5 +15,10 @@ setup(
     keywords=['storage', 's3', 'ftp', 'file', 'mongo', 'mongodb', 'fs', 'filesystem'],
     classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System',
                  'Topic :: Utilities'],
-    install_requires=install_requires
+    install_requires=install_requires,
+    extras_require={
+        "s3": [
+            "fs-s3fs==1.1.1"
+        ],
+    },
 )
