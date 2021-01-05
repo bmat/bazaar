0a8fb37ce3c0ae1ca2db3c449aae7ead58abf9ef
franco <fpelusso@bmat.com>
2021-01-05 13:21:35 -0300

    Update version.
    Related Issue: https://github.com/bmat/bazaar/issues/1
    After this commit, the issue seems to be completely solved.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as fp:
 setup(
     name='bazaar',
     packages=['bazaar'],
-    version='1.0',
+    version='1.0.1',
     description='Agnostic file storage',
     author='BMAT developers',
     author_email='tv-av@bmat.com',
